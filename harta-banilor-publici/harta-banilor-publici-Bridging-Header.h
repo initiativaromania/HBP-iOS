85ba469911cab73739089c0984ab79871deff0ea
Silviu Grijincu <silviu.grijincu@gmail.com>
2017-10-13 17:00:20 +0300

    Cleaned up headers file

--- harta-banilor-publici/harta-banilor-publici-Bridging-Header.h
+++ harta-banilor-publici/harta-banilor-publici-Bridging-Header.h
@@ -1,11 +1,3 @@
-//
-//  tabz-Bridging-Header.h
-//  tabz
-//
-//  Created by grijincu on 14/08/2017.
-//  Copyright © 2017 grijincu. All rights reserved.
-//
-
 #import "GMUMarkerClustering.h"
 #import "GMUKMLParser.h"
 #import "Google-Maps-iOS-Utils/GMUGeometryRenderer.h"
